fcfd71260b83e7537809ec1d9c83e6cf2257809c
B. August <benji889@yahoo.de>
2014-01-08 15:11:35 +0000

    Basics node added

---
+++ app.js
@@ -0,0 +1,12 @@
+var connect = require('connect')
+  , http = require('http')
+  , io = require('socket.io');
+
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static('public'));
+
+var server = http.createServer(app);
+
+io.listen(server);
+server.listen(8080);
